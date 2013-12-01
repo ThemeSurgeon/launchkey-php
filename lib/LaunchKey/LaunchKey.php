dba6fd87abb546bda44cd3a35d08b066aea8771e
Gabe Evans <gabe@ga.be>
2013-12-01 07:57:49 -0800

    Add ::configure() method

--- lib/LaunchKey/LaunchKey.php
+++ lib/LaunchKey/LaunchKey.php
@@ -10,4 +10,29 @@ class LaunchKey {
     // Current API version
     const API_VERSION = 'v1';
 
+    public static $config;
+
+    /**
+     * @internal
+     */
+    public static $_client;
+
+    public static function configure($options = array())
+    {
+        if ( ! self::$config)
+        {
+            self::$config = new Config;
+        }
+
+        self::$config->exchangeArray($options);
+        self::$_client = new Client;
+
+        return self::$config;
+    }
+
+    public static function __callStatic($method_name, $arguments)
+    {
+        call_user_func_array(self::$_client, $arguments);
+    }
+
 } // End LaunchKey
